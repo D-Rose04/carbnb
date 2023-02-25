2ac8d72991ccb4100ae7f94e3f1459cc6915b661
D-Rose04 <davidbhr04@gmail.com>
2023-02-25 16:40:30 -0400

    home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './templates/footer/footer.component';
 import { NavBarComponent } from './templates/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { Routes,RouterModule } from '@angular/router';
+import { GestionSolicitudesClientesComponent } from './components/gestion-solicitudes-clientes/gestion-solicitudes-clientes.component';
+
+
+const routes: Routes= [
+  {path:'',component:HomeComponent, pathMatch: 'full'},
+  {path:'gestion_solicitud',component:GestionSolicitudesClientesComponent, pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    GestionSolicitudesClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
